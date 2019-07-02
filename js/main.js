e80491301e5f3c116a9f7530c419a18acd4f4fdc
dzecyna <dzecyna@ukr.net>
2019-07-02 12:25:57 +0300

    commit

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-    document.getElementById('menu').onclick = function() {
+    document.getElementById('mobile-menu-input').onclick = function() {
         if (this.checked) {
             document.body.classList.add('overflow');
         } else {
@@ -14,5 +14,22 @@ $(document).ready(function() {
     $(".closer,.popup_overlay").click(function(){
         $(".popup,.popup_overlay").fadeOut(500);
     });
+
+    document.body.classList.add('visibility-shown');
+
+    const slider = $(".slider");
+
+    if (slider.length) {
+        slider.slick()
+    }
+
+    new DG.OnOffSwitch({
+        el: '#on-off-switch',
+        textOn: 'Individual',
+        textOff: 'Company',
+
+    });
+
+
 
 });
